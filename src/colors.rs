de8893dec719ba82659f0b9d1dd7f50e0ea9ca38
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-12-05 23:18:39 +0300

    feat: Add `colors` module

---
+++ src/colors.rs
@@ -0,0 +1,14 @@
+use owo_colors::OwoColorize;
+
+pub fn colors() -> String {
+    format!(
+        "{} {} {} {} {} {} {}",
+        "".black(),
+        "".red(),
+        "".yellow(),
+        "".green(),
+        "".cyan(),
+        "".blue(),
+        "".magenta(),
+    )
+}
