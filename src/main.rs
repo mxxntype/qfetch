bcbc66ffced9d30fdefb55544148f000aba2d885
mxxntype <59417007+mxxntype@users.noreply.github.com>
2023-12-05 23:18:48 +0300

    fix: Get hostname via `sysinfo`, use the new modules in `main()`

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
-mod hostname;
+mod colors;
+mod ram;
 mod user;
 
+use crate::{ram::Ram, user::User};
 use eyre::Result;
-
-use crate::{hostname::Hostname, user::User};
+use owo_colors::OwoColorize;
+use sysinfo::{RefreshKind, System, SystemExt};
 
 fn main() -> Result<()> {
     setup()?;
 
+    let system = System::new_with_specifics(RefreshKind::new().with_memory());
     let user = User::get();
-    let hostname = Hostname::get();
+    let ram = Ram::new(&system);
 
     println!();
-    println!("    user: {}", user);
-    println!("hostname: {}", hostname);
+    println!("\t󰀄  {user}");
+    println!(
+        "\t󰒋  {}",
+        system
+            .host_name()
+            .unwrap_or_else(|| "Unknown".into())
+            .magenta()
+    );
+    println!(
+        "\t󰹻  {}",
+        system.name().unwrap_or_else(|| "Unknown".into()).green()
+    );
+    println!("\t󰍛  {ram}");
+    println!("\t   {}", colors::colors());
     println!();
 
     Ok(())
