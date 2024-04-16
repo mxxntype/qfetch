e95868eba1b5f51669215d2319e23be3ad14da90
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-04-16 23:56:46 +0300

    chore: Add doc-comment for the `--icons` flag

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 
 #[derive(Debug, Default, Parser)]
 pub struct Config {
+    /// Use icons instead of textual prefixes for entries.
     #[arg(short, long)]
     pub icons: bool,
 }
